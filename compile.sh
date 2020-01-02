54a20ddc7325d0420ace93b5a6ae2a1c211101da
Lewis Crichton <lewistehminerz@outlook.com>
2020-01-02 19:57:18 +0000

    oop

--- compile.sh
+++ compile.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 OUT="p2.love"
-IGNORE=(".git/\*" ".github/\*" ".gitignore" ".vscode/\*" "compile.sh" "README.md")
+IGNORE=(".git/\\*" ".github/\\*" ".gitignore" ".vscode/\\*" "compile.sh" "README.md")
 
 FLAGS="-r $OUT ."
 for ignored in ${IGNORE[@]}; do
     FLAGS="$FLAGS -x $ignored"
 done
+
+echo compiling with: $FLAGS
 
 zip $FLAGS
